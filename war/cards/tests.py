7f1bfe9274510e4a003ef3bfccae989913736e54
Wilsonlin <wilson@wilsonlin.me>
2014-08-06 13:38:18 -0700

    Fix linting errors

--- war/cards/tests.py
+++ war/cards/tests.py
@@ -10,7 +10,7 @@ class BasicMathTestCase(TestCase):
     def test_math(self):
         a = 1
         b = 1
-        self.assertEqual(a+b, 2)
+        self.assertEqual(a + b, 2)
 
     # def test_failing_case(self):
     #     a = 1
@@ -72,17 +72,16 @@ class FormTestCase(TestCase):
         self.assertEqual(form.clean_username(), 'test-user')
 
 
-# class SyntaxTest(TestCase):
-#     def test_syntax(self):
-#         """
-#         Run pyflakes/pep8 across the code base to check for potential errors.
-#         """
-#         packages = ['cards']
-#         warnings = []
-#         # Eventually should use flake8 instead so we can ignore specific lines via a comment
-#         for package in packages:
-#             warnings.extend(run_pyflakes_for_package(package, extra_ignore=("_settings",)))
-#             warnings.extend(run_pep8_for_package(package, extra_ignore=("_settings",)))
-#         if warnings:
-#             self.fail("{0} Syntax warnings!\n\n{1}".format(len(warnings), "\n".join(warnings)))
-
+class SyntaxTest(TestCase):
+    def test_syntax(self):
+        """
+        Run pyflakes/pep8 across the code base to check for potential errors.
+        """
+        packages = ['cards']
+        warnings = []
+        # Eventually should use flake8 instead so we can ignore specific lines via a comment
+        for package in packages:
+            warnings.extend(run_pyflakes_for_package(package, extra_ignore=("_settings",)))
+            warnings.extend(run_pep8_for_package(package, extra_ignore=("_settings",)))
+        if warnings:
+            self.fail("{0} Syntax warnings!\n\n{1}".format(len(warnings), "\n".join(warnings)))
